d6f635f4e23e7e0312710cc50b1c4a6ade92c225
Aakash N S <aakashns@hey.com>
2020-09-17 19:31:26 +0530

    add download_raw_files

--- opendatasets/datasets/stackoverflow-developer-survey-2020/__init__.py
+++ opendatasets/datasets/stackoverflow-developer-survey-2020/__init__.py
@@ -1,14 +1,9 @@
-from opendatasets.utils import download_url, GITHUB_RAW_BASE_URL
+from opendatasets.utils import download_raw_files
 
-FILES = [
-    'survey_results_public.csv',
-    'survey_results_schema.csv',
-    'README.txt'
-]
 
 def download(dataset_id, data_dir, dry_run, **kwargs):
-    base_url = GITHUB_RAW_BASE_URL + dataset_id + "/"
-    for fname in FILES:
-        full_url = base_url + fname
-        download_url(full_url, data_dir, fname, dry_run=dry_run)
-            
+    download_raw_files(dataset_id, data_dir, dry_run, [
+        'survey_results_public.csv',
+        'survey_results_schema.csv',
+        'README.txt'
+    ])
--- opendatasets/utils/__init__.py
+++ opendatasets/utils/__init__.py
@@ -5,35 +5,12 @@ from tqdm import tqdm
 GITHUB_RAW_BASE_URL = 'https://raw.githubusercontent.com/JovianML/opendatasets/master/data/'
 
 
-def gen_bar_updater():
-    pbar = tqdm(total=None)
+def download_raw_files(dataset_id, data_dir, dry_run, files):
+    base_url = GITHUB_RAW_BASE_URL + dataset_id + "/"
+    for fname in files:
+        full_url = base_url + fname
+        download_url(full_url, data_dir, fname, dry_run=dry_run)
 
-    def bar_update(count, block_size, total_size):
-        if pbar.total is None and total_size:
-            pbar.total = total_size
-        progress_bytes = count * block_size
-        pbar.update(progress_bytes - pbar.n)
-
-    return bar_update
-
-def calculate_md5(fpath, chunk_size = 1024 * 1024):
-    md5 = hashlib.md5()
-    with open(fpath, 'rb') as f:
-        for chunk in iter(lambda: f.read(chunk_size), b''):
-            md5.update(chunk)
-    return md5.hexdigest()
-
-
-def check_md5(fpath, md5, **kwargs):
-    return md5 == calculate_md5(fpath, **kwargs)
-
-
-def check_integrity(fpath, md5=None):
-    if not os.path.isfile(fpath):
-        return False
-    if md5 is None:
-        return True
-    return check_md5(fpath, md5)
 
 def download_url(url, root, filename, md5=None, dry_run=False):
     """Download a file from a url and place it in root.
@@ -48,6 +25,7 @@ def download_url(url, root, filename, md5=None, dry_run=False):
     try:
         urlretrieve = urllib.request.urlretrieve
     except Exception:
+        # For Python 2.7
         urlretrieve = urllib.urlretrieve
 
     root = os.path.expanduser(root)
@@ -69,7 +47,8 @@ def download_url(url, root, filename, md5=None, dry_run=False):
                     url, fpath,
                     reporthook=gen_bar_updater()
                 )
-        except (urllib.error.URLError, IOError) as e:  # type: ignore[attr-defined]
+        # type: ignore[attr-defined]
+        except (urllib.error.URLError, IOError) as e:
             if url[:5] == 'https':
                 url = url.replace('https:', 'http:')
                 print('Failed download. Trying https -> http instead.'
@@ -83,3 +62,35 @@ def download_url(url, root, filename, md5=None, dry_run=False):
         # check integrity of downloaded file
         if not dry_run and not check_integrity(fpath, md5):
             raise RuntimeError("File not found or corrupted.")
+
+
+def gen_bar_updater():
+    pbar = tqdm(total=None)
+
+    def bar_update(count, block_size, total_size):
+        if pbar.total is None and total_size:
+            pbar.total = total_size
+        progress_bytes = count * block_size
+        pbar.update(progress_bytes - pbar.n)
+
+    return bar_update
+
+
+def calculate_md5(fpath, chunk_size=1024 * 1024):
+    md5 = hashlib.md5()
+    with open(fpath, 'rb') as f:
+        for chunk in iter(lambda: f.read(chunk_size), b''):
+            md5.update(chunk)
+    return md5.hexdigest()
+
+
+def check_md5(fpath, md5, **kwargs):
+    return md5 == calculate_md5(fpath, **kwargs)
+
+
+def check_integrity(fpath, md5=None):
+    if not os.path.isfile(fpath):
+        return False
+    if md5 is None:
+        return True
+    return check_md5(fpath, md5)
