7c043efa4a49412428defb91028e983ebcb46aa6
Aakash N S <aakashns@hey.com>
2020-10-06 15:21:38 +0530

    Add kaggle & click dependencies

--- setup.py
+++ setup.py
@@ -29,5 +29,5 @@ setuptools.setup(
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ),
-    install_requires=['tqdm']
+    install_requires=['tqdm', 'kaggle', 'click']
 )
